4f5f2da5af3fb3141dc868c237ad147e78b49390
gh-actions <gh-actions@users.noreply.github.com>
2025-12-18 06:07:08 +0000

    build

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ function App() {
     <div className="app">
       <div className="cat-container">
         <img
-          src="https://cdn2.thecatapi.com/images/ap2.jpg"
+          src="https://cdn2.thecatapi.com/images/882.jpg"
           alt="cat"
           className="cat-image"
         />
